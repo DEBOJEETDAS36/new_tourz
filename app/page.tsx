88ac5887f474cc38e8ad619e2e9c11809001698b
Debojeet Das <debojeetdas218@gmail.com>
2026-06-29 03:19:42 +0530

    Featured tours added in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/home/Hero";
+import FeaturedTours from "@/components/home/FeaturedTours";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <FeaturedTours />
     </div>
   );
 }
