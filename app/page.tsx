60f54e9366ae473f02459dfdaa97dc69317d91d7
Debojeet Das <debojeetdas218@gmail.com>
2026-06-29 03:24:05 +0530

    Popular destinations section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import FeaturedTours from "@/components/home/FeaturedTours";
+import PopularDestinations from "@/components/home/PopularDestinations";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <FeaturedTours />
+      <PopularDestinations />
     </div>
   );
 }
