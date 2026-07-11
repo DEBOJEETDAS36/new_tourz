c8720f985acc61c6cf841de3d95b808ed92d3b88
Debojeet Das <debojeetdas218@gmail.com>
2026-07-12 02:37:10 +0530

    Leisure home page created

---
+++ app/leisure/page.tsx
@@ -0,0 +1,16 @@
+import LeisureHero from '@/components/leisure/LeisureHero';
+import LeisureToursList from '@/components/leisure/LeisureToursList';
+
+export const metadata = {
+  title: 'Leisure Tours | Tourz',
+  description: 'Explore our curated leisure tour packages to amazing destinations',
+};
+
+export default function LeisurePage() {
+  return (
+    <div className="bg-[#0F2942]">
+      <LeisureHero />
+      <LeisureToursList />
+    </div>
+  );
+}
