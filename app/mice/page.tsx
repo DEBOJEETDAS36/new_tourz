1e4060519ac97af7fee95b3d07126cfa08b99115
Debojeet Das <debojeetdas218@gmail.com>
2026-07-12 02:53:19 +0530

    MICE Home page added

---
+++ app/mice/page.tsx
@@ -0,0 +1,16 @@
+import MiceHero from '@/components/mice/MiceHero';
+import MiceToursList from '@/components/mice/MiceToursList';
+
+export const metadata = {
+  title: 'MICE Events | Tourz',
+  description: 'Corporate meetings, incentive programs, conferences, and exhibitions',
+};
+
+export default function MicePage() {
+  return (
+    <div className="bg-[#0F2942]">
+      <MiceHero />
+      <MiceToursList />
+    </div>
+  );
+}
