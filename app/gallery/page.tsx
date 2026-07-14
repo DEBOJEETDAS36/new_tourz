094111c26dacb2244f653c2e44dfcafcf2a8ee8a
Debojeet Das <debojeetdas218@gmail.com>
2026-07-14 13:39:24 +0530

    Gallery route page created

---
+++ app/gallery/page.tsx
@@ -0,0 +1,16 @@
+import GalleryHero from '@/components/gallery/GalleryHero';
+import GalleryGrid from '@/components/gallery/GalleryGrid';
+
+export const metadata = {
+  title: 'Gallery | Tourz',
+  description: 'Stunning photography from our travel destinations',
+};
+
+export default function GalleryPage() {
+  return (
+    <div className="bg-[#0F2942]">
+      <GalleryHero />
+      <GalleryGrid />
+    </div>
+  );
+}
